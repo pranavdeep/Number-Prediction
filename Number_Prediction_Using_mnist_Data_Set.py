ee583f055b254f2bf619e32300918f36d824a2a7
pranavdeep <32843644+pranavdeep@users.noreply.github.com>
2019-01-02 13:04:07 +0530

    Add files via upload

---
+++ Number_Prediction_Using_mnist_Data_Set.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[66]:
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+# In[67]:
+
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+
+# In[68]:
+
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+
+# In[69]:
+
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy']) 
+model.fit(x_train, y_train, epochs=3)
+val_loss, val_acc = model.evaluate(x_test, y_test)
+
+
+# In[70]:
+
+
+""""model.save('epic_num_reader.model')
+new_model = tf.keras.models.load_model('epic_num_reader.model')"""
+predictions = model.predict(x_test)
+
+
+# In[80]:
+
+
+import numpy as np
+
+print(np.argmax(predictions[2]))
+
+
+# In[81]:
+
+
+plt.imshow(x_test[2],cmap=plt.cm.binary)
+plt.show()
+
